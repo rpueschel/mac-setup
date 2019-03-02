9a19fb7a18bb3bb35a6413a3f3b306d334ee5fc3
Robert Püschel <robert.pueschel@me.com>
2019-03-02 17:24:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# mac-setup
+Scripts to setup a new mac
+
+```bash
+git clone git@github.com:rpueschel/mac-setup.git
+cd mac-setup
+chmod a+x setup.sh
+./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/bin/sh -
+
+add_to_dock()
+{
+  defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/$1.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+}
+
+# ask for sudo upfront
+sudo -v
+
+# Install xcode command line tools
+xcode-select --install
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Install Brew Packages
+brew bundle install
+
+# remove all items from dock
+defaults write com.apple.dock persistent-apps -array
+
+# #add items to dock
+add_to_dock 'Google Chrome'
+add_to_dock 'iTerm'
+add_to_dock 'Atom'
+add_to_dock 'IntelliJ IDEA'
+add_to_dock 'DataGrip'
+add_to_dock 'PyCharm'
+add_to_dock 'GoLand'
+add_to_dock 'Slack'
+add_to_dock 'FaceTime'
+add_to_dock 'draw.io'
+add_to_dock 'Postman'
+add_to_dock 'FastViewer'
+add_to_dock 'Microsoft Excel'
+add_to_dock 'Microsoft OneNote'
+add_to_dock 'Microsoft PowerPoint'
+add_to_dock 'Microsoft Word'
+add_to_dock 'System Preferences'
+add_to_dock 'Mail'
+add_to_dock 'Calendar'
+killall Dock
